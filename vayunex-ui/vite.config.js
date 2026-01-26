3880eebb733e408e278dd84b0416670cfdc2c10d
futurespheretechnologies <vayunexsolution@gmail.com>
2026-01-26 16:08:22 +0530

    chore: enable HTTPS for local dev and fix sidebar syntax

--- vayunex-ui/vite.config.js
+++ vayunex-ui/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() /*, tailwindcss()*/],
+  plugins: [react(), tailwindcss(), basicSsl()],
   server: {
     port: 5173,
-    open: true
+    open: true,
+    https: true
   }
 })
